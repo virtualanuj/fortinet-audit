b01f8e238cd7b43ca5a2af96dfcc46f1faa477d5
Anuj Jain <virtualanuj@gmail.com>
2024-04-20 17:36:23 +0530

    Clean up

--- audit-service/src/main/java/com/fortinet/auditservice/controller/NotificationController.java
+++ audit-service/src/main/java/com/fortinet/auditservice/controller/NotificationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fortinet.auditservice.exceptions.NotificationNotFoundException;
@@ -17,6 +18,7 @@ import com.fortinet.auditservice.repository.NotificationRepository;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/v1")
 public class NotificationController {
 
     @Autowired
