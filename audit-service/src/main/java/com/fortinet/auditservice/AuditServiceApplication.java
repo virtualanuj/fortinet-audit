71b17225f98621138ada25ecc9d9903a7190a14c
Anuj Jain <virtualanuj@gmail.com>
2024-04-20 13:05:13 +0530

    Configured H2 database auditdb

--- audit-service/src/main/java/com/fortinet/auditservice/AuditServiceApplication.java
+++ audit-service/src/main/java/com/fortinet/auditservice/AuditServiceApplication.java
@@ -2,8 +2,9 @@ package com.fortinet.auditservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuditServiceApplication {
 
 	public static void main(String[] args) {
